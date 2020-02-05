e8f8248f4d9f8ab78c25d6559a585cabe7ab2a5b
Arthur Marques <abmarques.si@gmail.com>
2020-02-04 23:15:59 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -13,7 +13,8 @@ namespace SalesWebMvc.Controllers {
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Dev"] = "Arthur B. Marques";
 
             return View();
         }
