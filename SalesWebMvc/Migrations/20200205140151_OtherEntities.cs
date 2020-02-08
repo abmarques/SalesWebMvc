2233eb781f9729df6ad82856f9f8edcbbf1be7d1
Arthur Marques <abmarques.si@gmail.com>
2020-02-08 11:54:07 -0300

    Added Email attribute

--- SalesWebMvc/Migrations/20200205140151_OtherEntities.cs
+++ SalesWebMvc/Migrations/20200205140151_OtherEntities.cs
@@ -15,6 +15,7 @@ namespace SalesWebMvc.Migrations
                     Id = table.Column<int>(nullable: false)
                         .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
                     Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
                     BirthDate = table.Column<DateTime>(nullable: false),
                     BaseSalary = table.Column<double>(nullable: false),
                     DepartmentId = table.Column<int>(nullable: true)
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -7,6 +7,7 @@ namespace SalesWebMvc.Models {
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
@@ -16,9 +17,10 @@ namespace SalesWebMvc.Models {
 
         }
 
-        public Seller(int id, string name, DateTime birthDate, double baseSalary, Department department) {
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department) {
             Id = id;
             Name = name;
+            Email = email;
             BirthDate = birthDate;
             BaseSalary = baseSalary;
             Department = department;
